71ad966b91884eff21a547818c91bc1cf4930a00
Jane Sternbach <janesternbach@gmail.com>
2013-01-30 18:07:15 -0500

    added the subway hash

---
+++ mta.rb
@@ -0,0 +1,5 @@
+subways = {}
+subways[:n] = ["TimesSq", "34", "28n", "23n", "UnionSq", "8n"]
+subways[:six] = ["GrandCent", "33", "28s", "23s", "UnionSq", "8s"]
+subways[:l] = ["8Ave", "6Ave", "UnionSq", "3Ave", "1Ave"]
+
