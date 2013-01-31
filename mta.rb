6b88473a40b6952b8c4f133e6b28e6cd0be9a72f
Jane Sternbach <janesternbach@gmail.com>
2013-01-31 00:34:04 -0500

    final but a little prettier

--- mta.rb
+++ mta.rb
@@ -4,6 +4,7 @@ subways[:n] = ["timessq", "34", "28n", "23n", "unionsq", "8n"]
 subways[:six] = ["grandcent", "33", "28s", "23s", "unionsq", "8s"]
 subways[:l] = ["8ave", "6ave", "unionsq", "3ave", "1ave"]
 
+puts `clear`
 print "Which subway would you like to take? The N, 6 or L? "
 subwaychoice1 = gets.chomp.downcase
 if subwaychoice1 == "6"
@@ -16,10 +17,12 @@ else
   puts "You didn't enter a vaild choice"
 end
 
-print "Which stop would you like to get on at? #{subways[subwaychoice1]} "
+puts `clear`
+print "Which stop would you like to get on at? #{subways[subwaychoice1].join(", ")}: "
 on = gets.chomp.downcase
 puts "You didn't enter a stop" unless subways[subwaychoice1].include? on
 
+puts `clear`
 print "Which subway would you like to get off at? The N, 6, or L? "
 subwaychoice2 = gets.chomp.downcase
 if subwaychoice2 == "6"
@@ -32,7 +35,8 @@ else
   puts "You didn't enter a vaild choice"
 end
 
-print "Which stop would you like to get off? #{subways[subwaychoice2]} "
+puts `clear`
+print "Which stop would you like to get off? #{subways[subwaychoice2].join(", ")}: "
 off = gets.chomp.downcase
 puts "You didn't enter a stop" unless subways[subwaychoice2].include? off
 
@@ -49,7 +53,7 @@ else
   f = (d-e).abs
   c = g + f
 end
-
+puts `clear`
 puts "You are #{c} stops away from #{off}."
 
 
